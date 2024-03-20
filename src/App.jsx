60d09a232eb89f712fe43254616868e96816268d
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-03-20 20:11:45 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
